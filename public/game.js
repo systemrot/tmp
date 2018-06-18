314cbded284b6e971877f06a8f6c657bef16e7bd
Ros <hidan9000@yandex.ru>
2018-06-18 09:35:25 +0300

    Add files via upload

--- public/game.js
+++ public/game.js
@@ -45,6 +45,10 @@ let endOfGameAudio = document.createElement("audio");
 endOfGameAudio.src = "audio/endOfGame/applause.wav";
 endOfGameAudio.volume = 0.1;
 
+let comboAudio = document.createElement("audio");
+comboAudio.src = "audio/combo/spinnerspin.mp3";
+comboAudio.volume = 0.3;
+
 let GAME_OVER = false;
 let SCORE = 0;
 let LOST = 0;
@@ -59,8 +63,9 @@ let placeForTimer = {
 	y:50
 };
 
-n = 10;
+n = 25;// if you want to add more fruits - change the value!!!
 fruits = [];
+let timeForCombo = 0;
 let counter = 3;
 let opac = 1.0;
 let form = document.getElementById("tmp");
@@ -125,7 +130,7 @@ let startGame = function(login){
 	};
 
 	let getScore = function(){
-		context.font = "50px Verdana";
+		context.font = "50px BeerRegular";
 		var gradient = context.createLinearGradient(0,0,canvas.width, canvas.height);
 		gradient.addColorStop("0","white");
 		gradient.addColorStop("0.5","blue");
@@ -134,7 +139,10 @@ let startGame = function(login){
 		context.fillText("Score:" + SCORE,20,120);
 		context.fillText("Lost:" + LOST,20,170);
 		context.fillText("Time " + minutes + ":"+ seconds, placeForTimer.x, placeForTimer.y);
+		context.fillStyle = "#FF7900";
+		if(Math.round(timeForCombo) > 0)context.fillText("COMBO " + Math.round(timeForCombo), placeForTimer.x, placeForTimer.y + 50);
 		context.font = "20px Verdana";
+		context.fillStyle = gradient;
 		if(USER_NAME){context.fillText(USER_NAME, canvas.width - USER_NAME.length*15 , placeForTimer.y -10);}
 		context.fillText("F2:Pause",20,50);
 		context.fillText("Shif:Boost",20,25);
@@ -142,6 +150,7 @@ let startGame = function(login){
 	};
 
 	let SetCnvWH = function(){
+		dpy.style.height = canvas.height;
 		canvas.style.background = "url(img/Forest-31.jpg)";
 		canvas.style.backgroundRepeat = "no-repeat";
 		canvas.style.backgroundPosition = "top center"
@@ -149,6 +158,7 @@ let startGame = function(login){
 		dpyHeight = window.innerHeight - 6;
 		canvas.width = dpyWidth;
 		canvas.height = dpyHeight;
+		dpy.style.height = `${canvas.height}px`;
 		placeForTimer.x = canvas.width / 2 - 105;
 		context.clearRect(0,0, dpyWidth, dpyHeight);
 	};
@@ -204,14 +214,19 @@ let startGame = function(login){
 			if(this.y >= dpyHeight){
 				this.displayed = false;
 				LOST += 1;
+				timeForCombo = 0;
 			}
 		},
 
 		isCollision:function(girlX, girlY){
 			if(((this.y+this.height) >= girlY) && (this.y <= girlY+45) && (this.x >= girlX-45) && (this.x <= (girlX+150))){
 				this.displayed = false;
+				timeForCombo += 1.0;
 				catchingFruitsAudio.play();
-				SCORE+=1;
+				if(Math.round(timeForCombo) >= 2){
+					SCORE += Math.round(timeForCombo);
+				}
+					else SCORE+=1;
 			}
 		}
 	};
@@ -220,19 +235,23 @@ let startGame = function(login){
 		this.imgWidth = 321;
 		this.imgHeight = 213;
 		this.x = dpyWidth/2 - 50;
-		this.y = dpyHeight - 80;
+		this.y = dpyHeight - 83	;
 		this.v = 15;
 	};
 
 	Girl.prototype = {
+		setPosition:function(y){
+			this.y = y;
+		},
+
 		draw:function(){
-			context.drawImage(image2, 0, 20, this.imgWidth, this.imgHeight, this.x, this.y = dpyHeight - 80, 150, 90);
+			context.drawImage(image2, 0, 20, this.imgWidth, this.imgHeight, this.x, this.y, 150, 90);
 		},
 
 		moveTo:function(x){
 			this.x < x ? x-=58 : x -= 116;
 			if(x === this.x) return;
-			for(let i = 0; i < 30; i++){
+			for(let i = 0; i < 80; i++){
 				//this.x += 2;
 				x > this.x ? this.x += 1 : this.x -= 1;
 			}
@@ -280,106 +299,112 @@ let startGame = function(login){
 	let menu;
 	let btnSendScore;
 	let showRecords;
+	let getButtonsForEndGame = () => {
+		menu = document.createElement("div");
+		menu.style.width = "25em";
+		menu.style.background = "#33363F";
+		menu.style.marginTop = "5em";
+		menu.style.marginLeft = "auto";
+		menu.style.marginRight = "auto";
+		menu.style.height = "12em";
+		menu.style.borderRadius = "5px";
+		menu.style.boxShadow = "3px 3px 10px 3px #222222";
+		menu.style.opacity = "0.7";
+		dpy.appendChild(menu);
+		divScore = document.createElement("div");
+		inputScore = document.createElement("input");
+		inputScore.name = "score";
+		SCORE-=LOST;
+		inputScore.style.fontSize = "20px";
+		inputScore.value = "Score:" + SCORE;
+		inputScore.disabled = true;
+		inputScore.style.textAlign = "center";
+		inputScore.style.background = "white";
+		inputScore.style.height = "2em";
+		inputScore.style.width = "140px";
+		inputScore.style.borderRadius = "5px";
+		divScore.style.marginTop = "2em";
+		divScore.style.marginLeft = "auto";
+		divScore.style.marginRight = "auto";
+		divScore.style.height = "1em";
+		divScore.style.width = "5em";
+		divScore.style.boxShadow = "3px 3px 10px 3px #222222";
+		divScore.appendChild(inputScore);
+		menu.appendChild(divScore);
+		btnSendScore = document.createElement("div");
+		btnSendScore.style.width = "8em";
+		btnSendScore.style.fontSize = "20px";
+		btnSendScore.style.background = "white";
+		btnSendScore.style.marginTop = "2em";
+		btnSendScore.style.marginLeft = "auto";
+		btnSendScore.style.marginRight = "auto";
+		btnSendScore.style.height = "2em";
+		btnSendScore.style.borderRadius = "5px";
+		btnSendScore.style.boxShadow = "3px 3px 10px 3px #222222";
+		btnSendScore.style.paddingTop = "15px";
+		btnSendScore.style.textAlign = "center";
+		btnSendScore.innerHTML = "Send Score";
+		menu.appendChild(btnSendScore);
+		showRecords = document.createElement("div");
+		showRecords.style.fontSize = "20px";
+		showRecords.style.width = "8em";
+		showRecords.style.background = "white";
+		showRecords.style.marginTop = "1em";
+		showRecords.style.marginLeft = "auto";
+		showRecords.style.marginRight = "auto";
+		showRecords.style.height = "2em";
+		showRecords.style.borderRadius = "5px";
+		showRecords.style.boxShadow = "3px 3px 10px 3px #222222";
+		showRecords.innerHTML = "Show Records";
+		showRecords.style.paddingTop = "15px";
+		showRecords.style.textAlign = "center";
+		menu.appendChild(showRecords);
+		btnSendScore.onmouseenter = () => {
+			btnSendScore.style.background = "#111111";
+			btnSendScore.style.cursor = "pointer";
+		};
+		btnSendScore.onmouseleave = () => {
+			btnSendScore.style.background = "white";
+			btnSendScore.style.cursor = "default";
+		};
+		showRecords.onmouseenter = () => {
+			showRecords.style.background = "#111111";
+			showRecords.style.cursor = "pointer";
+		};
+		showRecords.onmouseleave = () => {
+			showRecords.style.background = "white";
+			showRecords.style.cursor = "default";
+		};
+		showRecords.onclick = () => {
+			alert("This feature does not work yet...");
+		};
+		btnSendScore.onclick = () => {
+			let xhr = new XMLHttpRequest();
+			xhr.open("POST", "/game/score", true);
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState != 4) return;
+				if (xhr.status != 200) {
+					alert(xhr.status + ': ' + xhr.statusText);
+				} else {
+					alert("ready");
+				}
+			};
+			xhr.send(`score=${SCORE}&login=${login}`);
+		};
+	};
+
+
 	let gameLoop = function(){
 		if(GAME_OVER){
 			cancelAnimationFrame(id);
 			canvas.remove();
-			menu = document.createElement("div");
-			menu.style.width = "55em";
-			menu.style.background = "#33363F";
-			menu.style.marginTop = "5em";
-			menu.style.marginLeft = "auto";
-			menu.style.marginRight = "auto";
-			menu.style.height = "35em";
-			menu.style.borderRadius = "5px";
-			menu.style.boxShadow = "3px 3px 10px 3px #222222";
-			menu.style.opacity = "0.7";
-			dpy.appendChild(menu);
-			divScore = document.createElement("div");
-			inputScore = document.createElement("input");
-			inputScore.name = "score";
-			SCORE-=LOST;
-			inputScore.style.fontSize = "20px";
-			inputScore.value = "Score:" + SCORE;
-			inputScore.disabled = true;
-			inputScore.style.textAlign = "center";
-			inputScore.style.background = "white";
-			divScore.style.marginTop = "2em";
-			divScore.style.marginLeft = "auto";
-			divScore.style.marginRight = "auto";
-			inputScore.style.height = "2em";
-			inputScore.style.width = "140px";
-			divScore.style.height = "2em";
-			divScore.style.width = "10em";
-			inputScore.style.borderRadius = "5px";
-			divScore.style.boxShadow = "3px 3px 10px 3px #222222";
-			divScore.appendChild(inputScore);
-			menu.appendChild(divScore);
-			btnSendScore = document.createElement("div");
-			btnSendScore.style.width = "8em";
-			btnSendScore.style.fontSize = "20px";
-			btnSendScore.style.background = "white";
-			btnSendScore.style.marginTop = "2em";
-			btnSendScore.style.marginLeft = "auto";
-			btnSendScore.style.marginRight = "auto";
-			btnSendScore.style.height = "2em";
-			btnSendScore.style.borderRadius = "5px";
-			btnSendScore.style.boxShadow = "3px 3px 10px 3px #222222";
-			btnSendScore.innerHTML = "Send Score";
-			btnSendScore.style.paddingTop = "15px";
-			btnSendScore.style.textAlign = "center";
-			menu.appendChild(btnSendScore);
-			showRecords = document.createElement("div");
-			showRecords.style.fontSize = "20px";
-			showRecords.style.width = "8em";
-			showRecords.style.background = "white";
-			showRecords.style.marginTop = "1em";
-			showRecords.style.marginLeft = "auto";
-			showRecords.style.marginRight = "auto";
-			showRecords.style.height = "2em";
-			showRecords.style.borderRadius = "5px";
-			showRecords.style.boxShadow = "3px 3px 10px 3px #222222";
-			showRecords.innerHTML = "Show Records";
-			showRecords.style.paddingTop = "15px";
-			showRecords.style.textAlign = "center";
-			menu.appendChild(showRecords);
-			btnSendScore.onmouseenter = () => {
-				btnSendScore.style.background = "#111111";
-				btnSendScore.style.cursor = "pointer";
-			};
-			btnSendScore.onmouseleave = () => {
-				btnSendScore.style.background = "white";
-				btnSendScore.style.cursor = "default";
-			};
-			showRecords.onmouseenter = () => {
-				showRecords.style.background = "#111111";
-				showRecords.style.cursor = "pointer";
-			};
-			showRecords.onmouseleave = () => {
-				showRecords.style.background = "white";
-				showRecords.style.cursor = "default";
-			};
-			showRecords.onclick = () => {
-				alert("This feature does not work yet...");
-			};
-			btnSendScore.onclick = () => {
-				let xhr = new XMLHttpRequest();
-				xhr.open("POST", "/game/score", true);
-				xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-				xhr.onreadystatechange = () => {
-					if (xhr.readyState != 4) return;
-					if (xhr.status != 200) {
-						alert(xhr.status + ': ' + xhr.statusText);
-					} else {
-						alert("ready");
-					}
-				};
-				xhr.send(`score=${SCORE}&login=${login}`);
-			};
+			getButtonsForEndGame();
 			return;
 		}
 		id = requestAnimationFrame(gameLoop);
 		SetCnvWH();
+		girl.setPosition(dpyHeight-80);
 		clearing();
 		getScore();
 		if(!mouseUp.value) girl.moveTo(mouseUp.pos);
@@ -389,6 +414,8 @@ let startGame = function(login){
 		});
 		if(!PAUSE && !END_OF_GAME){
 			timer();
+			/// -= 0.06  was a good way
+			if(timeForCombo > 0) timeForCombo -= 0.05
 			fruits.forEach(function(obj){
 				obj.isCollision(girl.x, girl.y);
 				obj.moving();
